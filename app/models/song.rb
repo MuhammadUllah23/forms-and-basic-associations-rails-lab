1052a4a15821514d6183897cc8e7e1f5915893b3
Muhammad <muhammadhullah23@gmail.com>
2021-07-16 17:07:54 -0400

    commit tree ed632db6bb850dce3c43b45cfd10775624b1283a

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,31 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+
+  def artist_name=(name)
+  
+  if !name.blank?
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(note)
+    
+    note.each do |n|
+      if !n.blank?
+        self.notes.build(content: n)
+        self.save
+      end
+    end
+  end
+
+
+  
 end
